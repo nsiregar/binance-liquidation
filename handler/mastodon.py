9e50898fc31d1211efe76448b3ea1cb529eaaf95
Ngalim Siregar <ngalim.siregar@gmail.com>
2023-07-22 07:22:51 +0700

    Update mastodon handler app creation

--- handler/mastodon.py
+++ handler/mastodon.py
@@ -2,14 +2,13 @@ from mastodon import Mastodon
 
 from config import config
 
-mastodon_app = Mastodon(
-    client_id=config.MASTODON_CLIENT_ID,
-    client_secret=config.MASTODON_CLIENT_SECRET,
-    access_token=config.MASTODON_ACCESS_TOKEN,
-    api_base_url=config.MASTODON_INSTANCE_URL,
-)
-
 
 class MastodonHandler:
+    def __init__(self) -> None:
+        self.app = Mastodon(
+            access_token=config.MASTODON_ACCESS_TOKEN,
+            api_base_url=config.MASTODON_INSTANCE_URL,
+        )
+
     def send_msg(self, msg):
-        mastodon_app.status_post(msg)
+        self.app.status_post(msg)
